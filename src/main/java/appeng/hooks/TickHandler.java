b4f02d787cad8b6881b0ff75f8edf0e3a2c86730
SST-03 <70825063+SST-03@users.noreply.github.com>
2025-08-19 13:47:35 +0800

    Update TickHandler.java

--- src/main/java/appeng/hooks/TickHandler.java
+++ src/main/java/appeng/hooks/TickHandler.java
@@ -143,18 +143,19 @@ public class TickHandler {
 
     @SubscribeEvent
     public void onChunkLoad(final ChunkEvent.Load load) {
-        boolean bool = false;
+        int i = 0;
         for (final Object te : load.getChunk().chunkTileEntityMap.values()) {
             if (te instanceof AEBaseTile) {
                 ((AEBaseTile) te).onChunkLoad();
-                bool = true;
+                i++;
             }
         }
-        if (bool) {
+        if (i > 0) {
             AELog.error(
-                    "[AppEng_Patch] I get Chunk Loaded event At x:%d z:%d. %d ticks",
+                    "[AppEng_Patch] I get Chunk Loaded event At x:%d z:%d Load %d times. %d ticks",
                     load.getChunk().xPosition,
                     load.getChunk().zPosition,
+                    i,
                     load.getChunk().worldObj.getWorldTime());
         }
     }
